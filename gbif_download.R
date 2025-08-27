7837c1cdaae751d61bd7b256943921af61d81be4
TerrellRoulston <terrellroulston@hotmail.ca>
2025-08-27 08:30:42 -0300

    added a few species to the script

--- gbif_download.R
+++ gbif_download.R
@@ -15,13 +15,18 @@ email='REDACTED'
 # Sporobolus michauxianus = 9592414
 # Sporobolus alterniflorus = 9394776
 # Sporobolus pumilus = 9478836 
-# Juncus gerradii = 6413621 
+# Juncus gerradii = 6413621
+# Juncus balticus = 2702048
 # Distichlis spicata = 2704509
 # Carex paleacea = 2727181 
+# Carex hormathodes = 2729422
 # Festuca rubra = 2704922
 # Plantago maritima = 3189755
 # Salicornia maritima = 5383953
 # Triglochin maritima = 2865368
+# Suaeda maritima = 3083541
+# Atriplex glabriuscula = 3083660
+# Atriplex patula = 3083728
 # Iva frutescens = 5402625
 
 # Sporobolus michauxianus = 9592414 ---------------------------------------
@@ -85,7 +90,7 @@ down_code = occ_download(
   pred("taxonKey", taxonKey),
   pred("basisOfRecord", basisOfRecord),
   pred("hasCoordinate", hasCoordinates),
-
+  
   format = "SIMPLE_CSV",
   user=user, pwd=pwd, email=email)
 
@@ -93,6 +98,23 @@ down_code = occ_download(
 occ_download_wait(down_code)
 download_jun_ger <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/jun_ger")
 
+
+# Juncus balticus = 2702048 -----------------------------------------------
+taxonKey <- 2702048
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_jun_ger <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/jun_bal")
 
 # Distichlis spicata = 2704509 --------------------------------------------
 taxonKey <- 2704509
@@ -127,6 +149,24 @@ down_code = occ_download(
 occ_download_wait(down_code)
 download_car_pal <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/car_pal")
 
+
+# Carex hormathodes = 2729422 ---------------------------------------------
+taxonKey <- 2729422
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_car_pal <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/car_hor")
+
+
 # Festuca rubra = 2704922 -------------------------------------------------
 taxonKey <- 2704922
 basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
@@ -142,6 +182,23 @@ down_code = occ_download(
 
 occ_download_wait(down_code)
 download_fus_rub <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/fes_rub")
+
+
+# Agrostis stolonifera = 2706435 ------------------------------------------
+taxonKey <- 2706435
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_fus_rub <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/agr_sto")
 
 
 # Plantago maritima = 3189755 ---------------------------------------------
@@ -194,6 +251,54 @@ down_code = occ_download(
 occ_download_wait(down_code)
 download_tri_mar <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/tri_mar")
 
+
+# Suaeda maritima = 3083541 -----------------------------------------------
+taxonKey <- 3083541
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_sau_mar <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/sau_mar")
+
+# Atriplex glabriuscula = 3083660 -----------------------------------------
+taxonKey <- 3083660
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_atr_gla <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/atr_gla")
+
+# Atriplex patula = 3083728 -----------------------------------------------
+taxonKey <- 3083728
+basisOfRecord <- c('HUMAN_OBSERVATION') # Human Observation for iNaturalist ONLY 
+hasCoordinates <- TRUE # limit to records with coordinates
+
+down_code = occ_download(
+  pred("taxonKey", taxonKey),
+  pred("basisOfRecord", basisOfRecord),
+  pred("hasCoordinate", hasCoordinates),
+  format = "SIMPLE_CSV",
+  user=user, pwd=pwd, email=email)
+
+
+occ_download_wait(down_code)
+download_atr_pat <- occ_download_get(down_code[1], overwrite = TRUE, path = "./occ_data/atr_pat")
 
 # Iva frutescens = 5402625 ------------------------------------------------
 taxonKey <- 5402625
